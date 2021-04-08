343db1090082961bae90d391d285a9d7a794b506
Walisson Silva <walissonsilva10@gmail.com>
2021-04-08 20:12:08 -0300

    chore: add sidebar component and graphics on Dashboard

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Input, Text, Icon } from '@chakra-ui/react'
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Input, Text, Icon, HStack, Box, Avatar } from '@chakra-ui/react'
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 
 export function Header() {
   return (
@@ -46,6 +46,33 @@ export function Header() {
         />
 
         <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+
+     <Flex align="center" ml="auto">
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+       <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Walisson Silva</Text>
+            <Text color="gray.300" fontSize="small">walissonsilva10@gmail.com</Text>
+          </Box>
+
+          <Avatar
+            size="md"
+            name="Walisson Silva"
+            src="https://avatars.githubusercontent.com/u/13500056?v=4" />
+        </Flex>
       </Flex>
     </Flex>
   );
